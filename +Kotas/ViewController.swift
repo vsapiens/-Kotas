bf85a91f768956ae709d79d7a0fbffe9083b47e0
Erick González <A01039859@itesm.mx>
2018-03-03 13:21:31 -0600

    Initial commit

---
+++ +Kotas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  +Kotas
+//
+//  Created by Erick González on 3/3/18.
+//  Copyright © 2018 Erick González. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
